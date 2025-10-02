4eb2f0d928099a85f1084e554b892a7010479312
fabian yataco <yatako1808@gmail.com>
2025-10-02 13:41:39 -0500

    Proyecto básico Express con MySql

---
+++ README.md
@@ -0,0 +1,29 @@
+# Procedimientos
+
+1. Clonar repositorio
+git clone https://...
+
+2. Restaurar la BD
+```sql
+CREATE DATABASE electrop Peru;
+USE electrop Peru;
+
+CREATE TABLE productos
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descripcion VARCHAR(50) NOT NULL,
+  garantia TINYINT NOT NULL,
+  precio DECIMAL(7,2) NOT NULL
+) ENGINE = INNODB;
+```
+
+3. Abrir proyecto electrop Peru en VSCode.
+
+4. Abrir la terminal CTRL + Ñ escribir:
+```
+npm install
+```
+
+Se ejecutará la instalación de todas las dependencias definidas en package.json
+
+Crear e ingresar los parámetros en el archivo .env
---
+++ routes/productoRoutes.js
@@ -0,0 +1,21 @@
+// RUTAS => acceso a los recursos
+// Verbos:
+// GET := Obtener , PUT := Actualizar , POST := crear , DELETE := eliminar
+const express = require('express')
+
+// Enrutador
+const router = express.Router()
+
+// Acceso := Crear, Listar, etc...
+const productoController = require('../controllers/productoController')
+
+// Definiendo las rutas
+router.post('/', productoController.crearProducto)
+
+router.get('/', productoController.obtenerProductos)
+
+router.get('/:id', productoController.obtenerProductoPorId)
+
+router.delete('/:id', productoController.eliminarProducto)
+
+module.exports = router
